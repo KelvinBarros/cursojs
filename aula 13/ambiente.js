fa485f71da9e4c2292b77a1439e215c8a7266bf6
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-25 18:21:13 -0300

    atualização na organização das pastas

--- aula 13/ambiente.js
+++ aula 13/ambiente.js
@@ -9,9 +9,19 @@ console.log("tudo bem?")
 console.log("tudo bem?")
  */
 
+
 var c =1
 
-while (c < 8){
-    console.log("tudo bem?")
+while (c <= 8){
+    console.log(`passo ${c}`)
     c++
 }
+/* 
+var c=1
+
+do {
+    console.log(`passo ${c}`)
+    c++
+    
+} while (c <= 8)
+*/
---
+++ aula 14/ambiente.js
@@ -0,0 +1,12 @@
+/*var c =1
+
+while (c <= 8){
+    console.log(`passo ${c}`)
+    c++
+}*/     
+console.log('vai começar...')
+for (c=1; c<=8; c++){/*1-INICIO 2-TESTE LOGI 3- INCREMENTO*/ 
+    console.log(`passo ${c}`)
+} 
+
+console.log("fim!")
