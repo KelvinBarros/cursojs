b359f964bf3400b696e232222b045539d367c875
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-25 16:26:47 -0300

    testes

---
+++ aula 13/ambiente.js
@@ -0,0 +1,17 @@
+/* 
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+ */
+
+var c =1
+
+while (c < 8){
+    console.log("tudo bem?")
+    c++
+}
