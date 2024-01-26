ca642c124704fc3a9a7e251d85ef54b39bfc3601
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-26 19:03:33 -0300

    iniciando exe 16

---
+++ aula 14 exe/exe 17/script.js
@@ -0,0 +1,18 @@
+
+
+function tabuada(){
+    let num = document.getElementById('num1')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        window.alert("[ERRO] VALOR NÃO PODE SER IGUAL A 0")
+    } else{
+        let n = Number(num.value)
+        let c = 1
+        while(c <= 10){
+            let item = document.createElement('option')
+
+        }
+    }
+    
+}
