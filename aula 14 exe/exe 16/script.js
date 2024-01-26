63658f67fbef84a838013b0fb7007395e2dc2039
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-25 23:45:23 -0300

    tentando resolver exercicio
    possivelmente ta tudo errado, mas é oq há por enquanto

--- aula 14 exe/exe 16/script.js
+++ aula 14 exe/exe 16/script.js
@@ -2,6 +2,15 @@ function contar(){
     var ini = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var pas = document.getElementById('passo')
+    var res = document.getElementById('res')
+
+    var n1 = Number(ini.value)
+    var n2 = Number(fim.value)
+    var n3 = Number(pas.value)
+
+    while(n1 <= n3){
+        res.innerHTML`${n1+n3}`
+    }
 
     
 }
