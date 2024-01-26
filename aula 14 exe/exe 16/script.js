73b3e12e2148f381a28e962922427b06f2f09168
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-26 18:31:55 -0300

    concluido exe15

--- aula 14 exe/exe 16/script.js
+++ aula 14 exe/exe 16/script.js
@@ -1,16 +1,45 @@
-function contar(){
+
+
+
+function contar() { /*PEGANDO O VALORES INSERIDOS */
     var ini = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var pas = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    var n1 = Number(ini.value)
-    var n2 = Number(fim.value)
-    var n3 = Number(pas.value)
+    //VERIFICANDO SE TODOS OS VALORES FORAM INSERIDOS
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        window.alert('[ERRO] Faltam dados. Ou Insira valores validos')
+    } else {
+        res.innerHTML = 'Contando...'
 
-    while(n1 <= n3){
-        res.innerHTML`${n1+n3}`
+
+        /*TRANSFORMANDO OS VALORES EM NUMBER*/
+        var n1 = Number(ini.value)
+        var n2 = Number(fim.value)
+        var n3 = Number(pas.value)
+        
+        if (n3 <= 0) {
+            window.alert('[ERRO] Passo não pode ser igual a 0. Considerando passo= 1 ')
+            
+        }
+
+        if (n1 < n2) {
+            for (var i = n1; i <= n2; i += n3) {
+                res.innerHTML += `${i} \u{1F449} `
+            } res.innerHTML += `\u{1F3C1}`
+
+
+        } else {
+            for (var i = n1; i >= n2; i -= n3) {
+                res.innerHTML += `${i} \u{1F449}  `
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }
+
+
     }
 
-    
+
+
 }
