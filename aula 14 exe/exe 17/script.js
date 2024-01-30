ec26d8b1e4b656ff1f44341408e2d51add7f902d
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-29 23:58:35 -0300

    finalizado exe017

--- aula 14 exe/exe 17/script.js
+++ aula 14 exe/exe 17/script.js
@@ -1,16 +1,24 @@
 
 
-function tabuada(){
+function tabuada(){//PEGAR AS VARIAVEIS 
     let num = document.getElementById('num1')
     let tab = document.getElementById('seltab')
 
+                //VERIFICAR SE O CAMPO ESTA PREENCHIDO
     if(num.value.length == 0){
         window.alert("[ERRO] VALOR NÃO PODE SER IGUAL A 0")
-    } else{
+    
+    } else{//TRANSFORMAR A VARIAVEL EM NUMERO, E REALIZAR O CALCULO
+
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML=''//LIMPAR TABUADA, TAB RECEBE NADA
         while(c <= 10){
-            let item = document.createElement('option')
+            let item = document.createElement('option')//CRIAR ELEMENTO 
+            item.text = `${n} x ${c}= ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)//ADIVIONAR ELEMENTO  COMO FILHO
+            c++
 
         }
     }
