3c8005b90fc733a5841861296662d571c2efefd0
KelvinBarros <kelvinbarros92@gmail.com>
2024-01-30 16:28:09 -0300

    Create ambiente.js

---
+++ aula 15/ambiente.js
@@ -0,0 +1,28 @@
+let num = [2,4,8,6,7]
+
+num [5]=9
+num.push(1)
+
+console.log(`nosso array é ${num}`)
+
+console.log(`nosso array tem ${num.length} elementos`)
+
+num.sort()
+console.log(`organizando nosso array em ordem cresceente ficará: ${num}`)
+
+
+
+for(let pos=0; pos<num.length; pos++){
+    console.log(`a posição ${pos} tem o valor ${num[pos]}`)
+}
+
+/*console.log('---------------------------------------')
+console.log('também é possivel fazer o mesmo, de forma diferente')
+
+for(let pos in num){
+    console.log(`a posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
+
+ let pos = num.indexOf(6)
+ console.log(pos)
