610070ef85a4348d221ee5b90a013809919f3455
KelvinBarros <kelvinbarros92@gmail.com>
2024-03-11 18:36:43 -0300

    Update script.js

---
+++ aula 16 exe/script.js
@@ -0,0 +1,28 @@
+
+let num = document.getElementById("num").value
+let tab = document.getElementById('tab')
+let res = document.getElementById('res')
+let valores = []
+
+function isNum(n)//paramos aqui
+
+
+
+
+
+
+
+
+
+
+
+function adicionar() {
+    
+if (isNum(num.value) && !inTab(num.value)){
+
+} else {
+    window.alert('Valor invalido ou já inserido')
+}
+
+    
+}
