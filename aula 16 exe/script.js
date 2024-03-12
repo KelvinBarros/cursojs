7fdab2ef8ffbfabf559b9e95a0fad8d933f9b4f2
KelvinBarros <kelvinbarros92@gmail.com>
2024-03-11 23:13:34 -0300

    Update script.js

--- aula 16 exe/script.js
+++ aula 16 exe/script.js
@@ -1,28 +1,31 @@
 
-let num = document.getElementById("num").value
+let num = document.getElementById("num")
 let tab = document.getElementById('tab')
 let res = document.getElementById('res')
-let valores = []
+let valores = [] //[] VETOR 
 
-function isNum(n)//paramos aqui
-
-
-
-
-
-
-
-
-
-
-
-function adicionar() {
-    
-if (isNum(num.value) && !inTab(num.value)){
-
-} else {
-    window.alert('Valor invalido ou já inserido')
+function isNum(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
 }
 
-    
+function inTab(n, t) {
+    if (t.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+
+    if (isNum(num.value) && !inTab(num.value, valores)) {
+
+    } else {
+        window.alert('Valor invalido ou já inserido')
+    }
+
 }
